49d3a88d18b58e02d23075d2e20d9b94304efd37
occook2 <119069061+occook2@users.noreply.github.com>
2024-06-13 17:51:30 -0400

    Created JWT Auth Service, Needs Updates

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+// import { CreateUserDto } from './dto/create-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+
+  // TODO: Remove hardcoded users table
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+  async findOne(username: string) {
+    return this.users.find(user => user.username === username);
+  }
+
+  // create(createUserDto: CreateUserDto) {
+  //   return 'This action adds a new user';
+  // }
+
+  // findAll() {
+  //   return `This action returns all users`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
+
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
