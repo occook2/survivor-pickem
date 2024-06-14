fc990b1a64ba74bcf122c4a412566e4c112cde8d
occook2 <119069061+occook2@users.noreply.github.com>
2024-06-14 14:59:29 -0400

    Added Custom Public Decorator and global jwt

--- server/src/auth/auth.guard.ts
+++ server/src/auth/auth.guard.ts
@@ -25,7 +25,7 @@ import {
             secret: jwtConstants.secret
           }
         );
-        // 💡 We're assigning the payload to the request object here
+        // We're assigning the payload to the request object here
         // so that we can access it in our route handlers
         request['user'] = payload;
       } catch {
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -6,6 +6,8 @@ import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
   imports: [
@@ -15,7 +17,12 @@ import { jwtConstants } from './constants';
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s'}, // Increase time for active JWT. Do additional research to determine timing (not in the scope of Nest documentation)
     })],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, 
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
