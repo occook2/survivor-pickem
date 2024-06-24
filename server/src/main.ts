e4925baeed28ce17d655052eb1327711a8bf5e50
occook2 <119069061+occook2@users.noreply.github.com>
2024-06-24 10:44:41 -0400

    Revert "Enable CORS"
    This reverts commit 10234b6cc9f6921779ac735b0d422f7d8d587c9e.

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,9 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
-
-  app.enableCors();
-  
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`)
 }
